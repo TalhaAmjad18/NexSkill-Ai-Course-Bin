0a1b873591cb608f3aca11b4b2a89cb90709e776
TalhaAmjad18 <mahrukhkaleem2@gmail.com>
2026-06-16 06:57:23 -0700

    practice 16 June 2026

---
+++ practice 16 june 2026.py
@@ -0,0 +1,18 @@
+# info is a string variable
+info = "Python is a brilliant programming language"
+
+# print info
+print(info)
+
+# print type of info
+print(type(info))
+
+# print length of info
+print(len(info))
+
+# print each character of info using for loop
+for i in info:
+    print(i)
+
+# print substring 'language'
+print(info[34::1])
