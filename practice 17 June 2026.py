5fa372f55f16fb0a1898e56c080044b3b63613f1
TalhaAmjad18 <mahrukhkaleem2@gmail.com>
2026-06-17 06:39:16 -0700

    practice 17 June 2026

---
+++ practice 17 June 2026.py
@@ -0,0 +1,11 @@
+bookYear = 1977
+print(bookYear)
+print(type(bookYear))
+
+bookPrice = 54.5
+print(bookPrice)
+print(type(bookPrice))
+
+result = bookYear * bookPrice
+print(result)
+print(type(result))
